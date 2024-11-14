bf4b6bd74c370609c28548d50c14d40b56358328
elchabin <nghbvinh19@gmail.com>
2024-11-14 14:29:55 +0700

    update: order management

---
+++ SaoCungDuocStore.Models/ViewModels/OrderVm.cs
@@ -0,0 +1,14 @@
+﻿using SaoCungDuocStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace SaoCungDuocStore.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
+    }
+}
