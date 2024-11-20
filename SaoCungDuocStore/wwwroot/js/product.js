9608c65dd93071c6ce0ac2eeb0d61297e74bfb55
unknown <vanha481@gmail.com>
2024-11-20 15:24:22 +0700

    sửa lại thành tiếng việt

--- SaoCungDuocStore/wwwroot/js/product.js
+++ SaoCungDuocStore/wwwroot/js/product.js
@@ -17,8 +17,8 @@ function loadDataTable() {
                 data: 'id',
                 "render": function (data) {
                     return `<div class="w-75 btn-group" role="group">
-                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i> Edit</a>               
-                     <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i> Delete</a>
+                     <a href="/admin/product/upsert?id=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i>Sửa</a>               
+                     <a onClick=Delete('/admin/product/delete/${data}') class="btn btn-danger mx-2"> <i class="bi bi-trash-fill"></i>Xoá</a>
                     </div>`
                 },
                 "width": "25%"
@@ -29,13 +29,13 @@ function loadDataTable() {
 
 function Delete(url) {
     Swal.fire({
-        title: 'Are you sure?',
-        text: "You won't be able to revert this!",
+        title: 'Bạn Có Chắc Không',
+        text: "Bạn sẽ không thể hoàn tác điều này!",
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
         cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes, delete it!'
+        confirmButtonText: 'Đúng rồi, xóa nó đi!'
     }).then((result) => {
         if (result.isConfirmed) {
             $.ajax({
