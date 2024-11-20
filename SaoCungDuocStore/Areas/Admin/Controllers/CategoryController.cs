8975f77541913ad2821a6ea4cd53422621f883ce
Nguyen Chau <hoangchau25102003@gmail.com>
2024-11-20 20:23:51 +0700

    Update Category and Company display

--- SaoCungDuocStore/Areas/Admin/Controllers/CategoryController.cs
+++ SaoCungDuocStore/Areas/Admin/Controllers/CategoryController.cs
@@ -8,7 +8,7 @@ using System.Diagnostics;
 namespace SaoCungDuocStore.Controllers
 {
     [Area("Admin")]
-   [Authorize(Roles = SD.Role_Admin)]
+    [Authorize(Roles = SD.Role_Admin)]
     public class CategoryController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
